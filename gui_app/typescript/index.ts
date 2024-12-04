8a293f47c92bc41ded9b0f90b0a7aa501434b234
RobertL31 <loicalban@laposte.net>
2024-12-04 15:48:06 +0100

    base of graph loading with button interaction

--- gui_app/typescript/index.ts
+++ gui_app/typescript/index.ts
@@ -1,29 +1,71 @@
 import * as SIGMA from "sigma";
 import * as GRAPH from "graphology";
 
-// Create a graphology graph
-const graph = new GRAPH.UndirectedGraph();
+const dummyModel = ` {
+  "processes": [
+    {
+      "pid": 1,
+      "name": "process1"
+    },
+    {
+      "pid": 2,
+      "name": "process2"
+    },
+    {
+      "pid": 3,
+      "name": "process3"
+    }
+  ]
+}`;
 
-graph.addNode("1", { label: "Node 1", x: 0, y: 0, size: 10, color: "blue" });
-graph.addNode("2", { label: "Node 2", x: 1, y: 1, size: 20, color: "red" });
-graph.addEdge("1", "2", { size: 5, color: "purple" });
 
-const graph_container = document.getElementById("graph-container");
-console.log(graph_container);
-if (!graph_container) {
-  throw new Error("Graph container not found");
+function parseModel(model: string): any {
+  return JSON.parse(model);
 }
 
-// Instantiate sigma.js and render the graph
-const sigmaInstance = new SIGMA.Sigma(graph, graph_container);
 
-const textbox = document.getElementById('textbox');
-if (!textbox) {
-  throw new Error("Textbox not found");
+function createGraph(model: any) {
+  const graph = new GRAPH.UndirectedGraph();
+
+  for (const process of model.processes) {
+    graph.addNode(`${process.pid}-${process.name}`, {x:Math.random(), y:Math.random(), size:10, color:"red"});  
+  }
+
+  return graph;
 }
 
-sigmaInstance.addListener('clickNode', function(e) {
-  const node = e.node;
-  textbox.innerHTML += `Node ${node.toString()} clicked</br>`;
-});
+
+function fillGraphContainer() {
+  const graph_container = document.getElementById("graph-container");
+  if (!graph_container) {
+    throw new Error("Graph container not found");
+  }
+
+  const model = parseModel(dummyModel);
+
+  const graph = createGraph(model);
+  var sigmaInstance = new SIGMA.Sigma(graph, graph_container);
+
+  const textbox = document.getElementById('textbox');
+  if (!textbox) {
+    throw new Error("Textbox not found");
+  }
+
+  sigmaInstance.addListener('clickNode', function(e) {
+    const node = e.node;
+    textbox.innerHTML += `${node.toString()} clicked</br>`;
+  });
+
+}
+
+const button = document.getElementById("setup-button");
+button?.addEventListener("click", () => fillGraphContainer());
+
+
+
+
+
+
+
+
 
