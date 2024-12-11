15ced18e4dc1b23981d53c0cb815c61a6fb9e25e
PoneyUHC <uhc.contact@laposte.net>
2024-12-11 14:14:45 +0100

    reindent typescript, webpack commandline option

--- gui_app/typescript/index.ts
+++ gui_app/typescript/index.ts
@@ -3,20 +3,20 @@ import * as GRAPH from "graphology";
 
 
 const dummyModel = ` {
-  "processes": [
-    {
-      "pid": 1,
-      "name": "process1"
-    },
-    {
-      "pid": 2,
-      "name": "process2"
-    },
-    {
-      "pid": 3,
-      "name": "process3"
-    }
-  ]
+    "processes": [
+        {
+            "pid": 1,
+            "name": "process1"
+        },
+        {
+            "pid": 2,
+            "name": "process2"
+        },
+        {
+            "pid": 3,
+            "name": "process3"
+        }
+    ]
 }`;
 
 
@@ -25,94 +25,93 @@ var global_model: JSON;
 
 
 function createGraph(model: any) {
-  const graph = new GRAPH.UndirectedGraph();
+    const graph = new GRAPH.DirectedGraph();
 
-  for (const file of model.files) {
-    var file_label = file.path;
-    graph.addNode(file_label, {x:Math.random(), y:Math.random(), size:10, color:"green", label: file_label});
-  }
-
-  for (const process of model.processes) {
-    var process_label = `${process.pid}-${process.name}`;
-    graph.addNode(process_label, {x:Math.random(), y:Math.random(), size:10, color:"red", label: process_label});  
-    
-    for (const open_info of process.open_infos) {
-      const file = model.files[open_info.file];
-      var file_label = file.path;
-      graph.addEdge(process_label, file_label, {color:"black"});
+    for (const file of model.files) {
+        var file_label = file.path;
+        graph.addNode(file_label, { x: Math.random(), y: Math.random(), size: 10, color: "green", label: file_label });
     }
-  }
 
-  return graph;
+    for (const process of model.processes) {
+        var process_label = `${process.pid}-${process.name}`;
+        graph.addNode(process_label, { x: Math.random(), y: Math.random(), size: 10, color: "red", label: process_label });
+
+        for (const open_info of process.open_infos) {
+            const file = model.files[open_info.file];
+            var file_label = file.path;
+            graph.addEdge(process_label, file_label, { color: "black" });
+        }
+    }
+
+    return graph;
 }
 
 
 function getFileContent() {
-  var input = document.createElement('input');
-  input.type = 'file';
+    var input = document.createElement('input');
+    input.type = 'file';
 
-  input.onchange = e => { 
+    input.onchange = e => {
 
-    if (! e.target){
-      return;
+        if (!e.target) {
+            return;
+        }
+
+        var inputElement = e.target as HTMLInputElement;
+        if (!inputElement || !inputElement.files) {
+            throw new Error("File input or files not found");
+        }
+
+        var file = inputElement.files[0];
+
+        if (!file) {
+            throw new Error("File not found");
+        }
+
+        global_model_filename = file.name;
+
+        const filename_textbox = document.getElementById("filename");
+        if (!filename_textbox) {
+            throw new Error("Filename textbox not found");
+        }
+
+        filename_textbox.innerHTML = `Current file : ${global_model_filename}`;
+
+
+        var reader = new FileReader();
+        reader.readAsText(file, 'UTF-8');
+
+
+        reader.onload = readerEvent => {
+            var content = readerEvent.target?.result;
+            global_model = JSON.parse(content as string);
+        }
+
     }
 
-    var inputElement = e.target as HTMLInputElement;
-    if (!inputElement || !inputElement.files) {
-      throw new Error("File input or files not found");
-    }
-    
-    var file = inputElement.files[0];
-    
-    if( !file ){
-      throw new Error("File not found");
-    }
-    
-    global_model_filename = file.name;
-
-    const filename_textbox = document.getElementById("filename");
-    if (!filename_textbox) {
-      throw new Error("Filename textbox not found");
-    }
-
-    filename_textbox.innerHTML = `Current file : ${global_model_filename}`;
-
-
-    var reader = new FileReader();
-    reader.readAsText(file,'UTF-8');
-
-
-    reader.onload = readerEvent => {
-        var content = readerEvent.target?.result;
-        global_model = JSON.parse(content as string);
-    }
-
-  }
-
-  input.click();
+    input.click();
 }
 
 
 async function fillGraphContainer() {
 
-  const graph_container = document.getElementById("graph-container");
-  if (!graph_container) {
-    throw new Error("Graph container not found");
-  }
+    const graph_container = document.getElementById("graph-container");
+    if (!graph_container) {
+        throw new Error("Graph container not found");
+    }
 
-  const graph = createGraph(global_model);
-  var sigmaInstance = new SIGMA.Sigma(graph, graph_container);
-  var dragListener = new sigmaInstance.
+    const graph = createGraph(global_model);
+    var sigmaInstance = new SIGMA.Sigma(graph, graph_container);
 
-  const textbox = document.getElementById('textbox');
-  if (!textbox) {
-    throw new Error("Textbox not found");
-  }
+    const textbox = document.getElementById('textbox');
+    if (!textbox) {
+        throw new Error("Textbox not found");
+    }
 
-  sigmaInstance.addListener('clickNode', function(e) {
-    const node = e.node;
-    textbox.innerHTML += `${node.toString()} clicked</br>`;
-  });
+    sigmaInstance.addListener('clickNode', function (e) {
+        const node = e.node;
+        textbox.innerHTML += `${node.toString()} clicked</br>`;
+    });
 }
 
 const setup_button = document.getElementById("setup-button");
