6c0898b6f49ba75c7ec7443c1b07c2b59f97513d
RobertL31 <loicalban@laposte.net>
2025-02-07 11:01:27 +0100

    bash to python transition wip

--- src/ipc_analyzer/scripts/run.bash
+++ src/ipc_analyzer/scripts/run.bash
@@ -15,11 +15,6 @@ fi
 
 for ((i = 0 ; i < $n_exec ; i++ ));
 do
-<<<<<<< HEAD:scripts/run.bash
-    ./scripts/build.bash
-    ./scripts/exec.bash report${printf "%02d" $i}.json ${wait_time};
-=======
     source ${SCRIPT_DIR}/build.bash
     source ${SCRIPT_DIR}/exec.bash report${i}.json ${wait_time}; 
->>>>>>> 3e385ac (bash to python transition wip):src/ipc_analyzer/scripts/run.bash
 done
